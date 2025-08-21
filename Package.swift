f64f38b480353d022f2df32af347351b3f310636
Frizlab <fload@me.com>
2025-08-21 16:43:21 +0200

    Decrease the minimum platforms for the package
    Async has been backported to previous OS versions.

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
 	name: "AsyncOperations",
-	platforms: [.macOS(.v12), .iOS(.v15), .tvOS(.v15), .watchOS(.v8)],
+	platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6)],
 	products: [.library(name: "AsyncOperations", targets: ["AsyncOperations"])],
 	dependencies: [
 		.package(url: "https://github.com/happn-app/RetryingOperation.git", from: "1.1.7")
